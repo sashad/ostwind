51f17ae09613874fd1ff9e0a6f06b1a32d2a12f8
Aleksandr Demidov <demidoff@1vp.ru>
2024-11-06 11:38:26 +0000

    start

---
+++ ostwind_rent/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Rental Menegment System",
+    "version": "17.0.0.0.1",
+    "category": "Services",
+    "license": "AGPL-3",
+    "author": "Ostwind Software",
+    "website": "https://ostwind.i2base.com",
+    "depends": ["base", "account", "product", "portal"],
+    "development_status": "Development/Unstable",
+    "data": [
+#        "data/contract_cron.xml",
+#        "views/contract_portal_templates.xml",
+    ],
+    "assets": {
+#        "web.assets_frontend": ["contract/static/src/scss/frontend.scss"],
+#        "web.assets_tests": ["contract/static/src/js/contract_portal_tour.esm.js"],
+    },
+    "installable": True,
+}
